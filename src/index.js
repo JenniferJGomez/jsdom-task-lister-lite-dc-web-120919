02c515bf0edf15dde77dcd7507e82f5742fd122e
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-01-29 10:09:48 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submitForm = document.getElementById("create-task-form")
+
+  submitForm.addEventListener('submit', submitHandler)
+
 });
+
+function submitHandler(event) {
+  event.preventDefault()
+  let newTask = document.getElementById("new-task-description").value 
+  event.target.reset()
+  // add task function
+}
+
+function getTasks() {
+  return document.getElementById("tasks")
+}
