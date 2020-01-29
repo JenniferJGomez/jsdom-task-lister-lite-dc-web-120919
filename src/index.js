9b04ca2bb4a01ead85e8232c8bf35bbe6b0962fa
github username <github email address>
2020-01-29 14:25:14 -0500

    Delete button on list

--- src/index.js
+++ src/index.js
@@ -8,31 +8,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function submitHandler(event) {
   event.preventDefault()
-  let userInput = document.getElementById("new-task-description").value 
+  let userInput = document.getElementById("new-task-description").value
   event.target.reset()
-  
-  addTask(userInput)
-  // add task function
+
+  let liTask = addTask(userInput)
+  addButton(liTask)
 }
 
 function getTasks() {
   return document.getElementById("tasks")
 }
 
-function addTask(userInput){ 
-
+function addTask(userInput){
   let task = getTasks()
   let newTask = makeNewTask(userInput)
  task.appendChild(newTask)
+ return task
 }
 
 
-function makeNewTask(userInput) { 
-  
+
+function makeNewTask(userInput) {
+
   let newLi = document.createElement("li")
-  // let newTask = document.getElementById("new-task-description").value 
+  let button = document.createElement("button")
+
+  // let newTask = document.getElementById("new-task-description").value
   //  let x = document.createTextNode(userInput)
    newLi.innerText = userInput
-  
+
   return newLi
 }
+
+function addButton(liTask) {
+    let span = document.createElement('SPAN')
+    let text = document.createTextNode('\u00D7')
+    span.appendChild(text)
+    liTask.appendChild(span)
+  }
